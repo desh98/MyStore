9bb8efa9bded3a9cc7921a9b0c4ee706aa1d19f3
desh98 <deshaniabey826@gmail.com>
2023-10-31 00:38:36 -0700

    Images added.

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,31 @@
-// App.jsx
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 import ProductList from './components/ProductList';
 
-const productsData = [
-  // {
-  //   id: 1,
-  //   name: 'Product 1',
-  //   price: 29.99,
-  //   image: './assets/a1.jpg',
-  // },
-  // {
-  //   id: 2,
-  //   name: 'Product 2',
-  //   price: 39.99,
-  //   image: './assets/b1.jpg',
-  // },
-  // {
-  //   id: 3,
-  //   name: 'Product 3',
-  //   price: 49.99,
-  //   image: './assets/c1.jpg',
-  // },
-];
+const productsData = [];
 
 const App = () => {
   const [products, setProducts] = useState(productsData);
 
   return (
     <div className="min-h-screen bg-gray-100 text-white font-inter font-normal dark:bg-gray-900">
-      <div className="flex flex-row">
-        <div  className="flex flex-col justify-start bg-slate-600">
-          <h1 className="flex font-bold text-4xl mb-8 p-24">MyStore</h1>
-          <img src='./assets/ecom.png' className="w-64 h-32" />
+      <div className="flex flex-col lg:flex-row">
+        <div className="lg:w-1/3 bg-slate-600 flex flex-col items-center h-auto p-8">
+          <h1 className="font-bold text-4xl mb-8 p-24">MyStore</h1>
+          <motion.img 
+            src="/assets/ecom.png" 
+            className="w-64 h-auto mb-12" 
+            alt="Ecommerce"
+            initial={{ x: '-100vw' }}
+            animate={{ x: 0 }}
+            transition={{ duration: 2, ease: 'easeInOut' }} 
+          />
         </div>
-        <ProductList products={products} />
+        <div className="lg:w-2/3 p-24">
+          <ProductList products={products} />
+        </div>
       </div>
     </div>
-    
   );
 };
 
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,18 +1,31 @@
-// ProductList.js
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 import productsData from '../Products.json';
+
+const ProductItem = ({ product }) => {
+  return (
+    <motion.div
+      className="bg-white shadow-lg rounded-lg overflow-hidden"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <img src={product.src} alt={product.name} className="w-full h-48 object-cover object-center" />
+      <div className="p-4 flex flex-col">
+        <div className="flex justify-between mb-1">
+          <h3 className="text-gray-800 font-bold text-md">{product.name}</h3>
+          <p className="text-gray-600">${product.price.toFixed(2)}</p>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
 
 const ProductList = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 p-12 justify-center items-center">
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
       {productsData?.Products?.map(product => (
-        <div key={product.id} className="bg-white space-x-4 shadow-lg rounded-lg overflow-hidden w-max">
-          <img src={product.src} alt={product.name} className="w-64 h-32 object-cover object-center" />
-          <div className="p-4">
-            <h3 className="text-gray-800 font-bold text-lg mb-1">{product.name}</h3>
-            <p className="text-gray-600">${product.price.toFixed(2)}</p>
-          </div>
-        </div>
+        <ProductItem key={product.id} product={product} />
       ))}
     </div>
   );
