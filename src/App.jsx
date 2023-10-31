5479a77b98470bb5d8fd09ac7e087a176fa75734
desh98 <deshaniabey826@gmail.com>
2023-10-31 07:32:59 -0700

    Product => hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,8 @@ const App = () => {
     <div className="min-h-screen bg-gray-100 text-white font-inter font-normal dark:bg-gray-900">
       <div className="flex flex-col lg:flex-row">
         <div className="lg:w-1/3 bg-slate-600 flex flex-col items-center h-auto p-8">
-          <h1 className="font-bold text-4xl mb-8 p-24">MyStore</h1>
+          <h1 className="font-bold text-4xl mb-8 p-12 mt-12">MyStore</h1>
+
           <motion.img 
             src="/assets/ecom.png" 
             className="w-64 h-auto mb-12" 
@@ -21,7 +22,7 @@ const App = () => {
             transition={{ duration: 2, ease: 'easeInOut' }} 
           />
         </div>
-        <div className="lg:w-2/3 p-24">
+        <div className="lg:w-2/3 p-4 md:p-24">
           <ProductList products={products} />
         </div>
       </div>
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,8 +1,30 @@
-import React, { useRef, useEffect } from 'react';
-import { motion, useAnimation } from 'framer-motion';
+import React, { useState, useEffect, useRef } from 'react';
+import { motion } from 'framer-motion';
 import productsData from '../Products.json';
 
 const ProductItem = ({ product }) => {
+  const [hoveredImageIndex, setHoveredImageIndex] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (product.src && product.src.length > 1) {
+      interval = setInterval(() => {
+        setHoveredImageIndex((prevIndex) => (prevIndex + 1) % product.src.length);
+      }, 3000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [product.src]);
+
+  useEffect(() => {
+    if (hoveredImageIndex === 2) { // Reset to the first image after the 3rd image is hovered (assuming 0-based index)
+      setHoveredImageIndex(0);
+    }
+  }, [hoveredImageIndex]);
+
+  
   return (
     <motion.div
       className="bg-white shadow-lg rounded-lg overflow-hidden"
@@ -10,21 +32,54 @@ const ProductItem = ({ product }) => {
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
     >
-      <img src={product.src} alt={product.name} className="w-full h-48 object-cover object-center" />
-      <div className="p-4 flex flex-col">
+      <img
+      src={product.src && product.src.length > 0 ? product.src[hoveredImageIndex] : ''}
+      alt={product.name}
+      className="w-full h-24 object-cover object-center"
+    />
+      <div className="p-2 flex flex-col">
         <div className="flex justify-between mb-1">
-          <h3 className="text-gray-800 font-bold text-md">{product.name}</h3>
-          <p className="text-gray-600">${product.price.toFixed(2)}</p>
+          <h3 className="text-gray-800 font-bold text-xs">{product.name}</h3>
+          <p className="text-gray-600 text-xs">LKR {product.price.toFixed(2)}</p>
         </div>
       </div>
     </motion.div>
   );
 };
 
+
 const ProductList = () => {
+  // const [visibleProducts, setVisibleProducts] = useState(4);
+  const productsContainerRef = useRef(null);
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     (entries) => {
+  //       entries.forEach((entry) => {
+  //         if (entry.isIntersecting) {
+  //           setVisibleProducts((prevCount) => prevCount + 4); 
+  //         }
+  //       });
+  //     },
+  //     {
+  //       threshold: 0.1,
+  //     }
+  //   );
+
+    // if (productsContainerRef.current) {
+    //   observer.observe(productsContainerRef.current);
+    // }
+
+  //   return () => {
+  //     observer.disconnect();
+  //   };
+  // }, []);
+
+  const Product = productsData?.Products?.slice(0);
+
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {productsData?.Products?.map(product => (
+    <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4" ref={productsContainerRef}>
+      {Product?.map((product) => (
         <ProductItem key={product.id} product={product} />
       ))}
     </div>
